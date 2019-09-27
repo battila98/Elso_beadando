c5a7da787333cf2f066e3073dfb5e545186c455a
ASD <battila98@gmail.com>
2019-09-27 10:54:39 +0200

    Első feladat elkészítése

--- Elso_Beadando/Program.cs
+++ Elso_Beadando/Program.cs
@@ -8,8 +8,40 @@ namespace Elso_Beadando
 {
     class Program
     {
+        static List<int> lista = new List<int>();
+        
+
+
+        //var end
         static void Main(string[] args)
         {
+            feladat1();
+
+
+
+            Console.ReadLine();
         }
+        //**Main end
+        static void feladat1()
+        {
+            int max = 0;
+            Console.WriteLine("Adjon meg 10 számot!");
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (max < lista[i]) 
+                {
+                    max = lista[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám a {0} volt",max);
+
+        }
+
+
     }
 }
